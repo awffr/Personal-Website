8de0fd50778a11a307df0343b7aab39aebbc00ae
awffr <awfafarhan78@gmail.com>
2024-08-26 20:42:12 +0700

    feat: add project landing page

--- Awfa-Farhan/src/App.tsx
+++ Awfa-Farhan/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Experiences from './pages/Experiences'; 
 import Construction from './pages/Construction'; 
 import Stats from './pages/Stats'; 
+import Projects from './pages/Projects'; 
 
 import Menu from './components/Menu';  
 import MenuWrapper from './components/MenuWrapper';  
@@ -27,7 +28,7 @@ const AppContent = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Experiences" element={<Experiences />} />
-        <Route path="/Projects" element={<Construction />} />
+        <Route path="/Projects" element={<Projects />} />
         <Route path="/Journey" element={<Construction />} />
         <Route path="/Stats" element={<Stats />} />
         <Route path="/Blog" element={<Construction />} />
--- Awfa-Farhan/src/pages/Projects.tsx
+++ Awfa-Farhan/src/pages/Projects.tsx
@@ -1,9 +1,56 @@
+import Header from '../components/Header';
 import React from 'react';
 
 const Projects: React.FC = () => {
+
+  const projects = [
+    { id: 1, name: 'Keythicc', display: '/project-dipslay-keythicc.webp', imageSrc: '/project-image-keythicc.webp', description:'Customizable keyboard commerce', icon:'/project-icon-keythicc.svg', color: '#4F17F7' },
+    { id: 2, name: 'Triage', display: '/project-image-triage.webp', imageSrc: '/project-image-triage.webp', description:'Java emergency queue program', icon:'/project-icon-triage.svg', color: '#3AF09E'},
+    { id: 3, name: 'Nashua', display: '/project-image-nashua.webp', imageSrc: '/project-image-nashua.webp', description:'Tape branding website', icon:'/project-icon-nashua.svg', color: '#FCB910'},
+  ];
+  
   return (
-    <div>
-      <h1>Welcome to the Projects Page</h1>
+    <div className="bg-[#08090A] min-h-screen flex flex-col relative overflow-clip">
+      <Header/>
+
+      <section id="project" className='mx-[100px] '>
+        <div id="project-content" className="my-16 flex flex-col items-center gap-y-10">
+          {projects.map(({ id, name, display, description, icon, color }) => (
+            <div
+              key={id}
+              className="w-[90%] p-2 bg-[#0D0D0D] text-white hover:cursor-pointer border border-zinc-800 rounded-xl relative overflow-hidden transition-colors duration-500"
+            >
+              <div className="my-5 flex px-12">
+                <img 
+                  src={icon} 
+                  alt={`Project Image ${name}`}
+                  className="w-auto h-auto"
+                />
+                <div className="ml-4">
+                  <h2 className="font-semibold text-lg text-[#CECECE]">
+                    {name}
+                  </h2>
+                  <p className="font-regular text-[16px] text-[#959595]">
+                    {description}
+                  </p>
+                </div>
+              </div>
+              <div className="overflow-hidden mb-8">
+                <img 
+                  src={display} 
+                  alt={`Project Display ${name}`}
+                  className="mx-auto w-[90%] h-auto transform transition-transform duration-500 hover:opacity-60"
+                />
+              </div>
+              {/* Subtle gradient background effect based on color prop */}
+              <div
+                className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-600"
+                style={{ background: `linear-gradient(to top, ${color}20 10%, transparent)` }}
+              ></div>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
